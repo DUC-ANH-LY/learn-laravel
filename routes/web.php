173afa49d62366a9011be8609bd461bf3edfe309
DUC-ANH-LY <namobilegame@gmail.com>
2024-11-17 21:32:48 +0700

    ex 3.10 3.11 3.12

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 
-// with url helper
-/*<a href="<?php echo url('/'); ?>">*/
-//// Outputs <a href="http://myapp.com/">
+Route::group([],function () {
+    Route::get('hello', function () {
+        return 'Hello';
+    });
+    Route::get('world', function () {
+        return 'World';
+    });
+});
+// learn more about middleware in Chapter 10
+Route::middleware('auth')->group(function() {
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    });
+    Route::get('account', function () {
+        return view('account');
+    });
+});
 
-
-// Defining a route with name() in routes/web.php:
-Route::get('members/{id}', 'MembersController@show')->name('members.show');
-// Linking the route in a view using the route() helper:
-// on view blade template
-/*<a href="<?php echo route('members.show', ['id' => 14]); ?>">*/
+// rate limit ( hạn chế (limit) số lượng request gửi/nhận (rate))
+// first: number of request allowed ,
+// second: number of minutes to wait before resetting the attempt count
+Route::middleware('auth:api', 'throttle:60,1')->group(function () {
+    Route::get('/profile', function () {
+        //
+    });
+});
