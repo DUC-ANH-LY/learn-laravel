d57bf3321536dc0e540d311660fb814b79bcf536
DUC-ANH-LY <namobilegame@gmail.com>
2024-11-17 21:35:35 +0700

    ex 3.13-15

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,34 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::group([],function () {
-    Route::get('hello', function () {
-        return 'Hello';
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', function () {
+        // Handles the path /dashboard
+        return 'dashboard';
     });
-    Route::get('world', function () {
-        return 'World';
-    });
-});
-// learn more about middleware in Chapter 10
-Route::middleware('auth')->group(function() {
-    Route::get('dashboard', function () {
-        return view('dashboard');
-    });
-    Route::get('account', function () {
-        return view('account');
+    Route::get('users', function () {
+        return 'user';
     });
 });
 
-// rate limit ( hạn chế (limit) số lượng request gửi/nhận (rate))
-// first: number of request allowed ,
-// second: number of minutes to wait before resetting the attempt count
-Route::middleware('auth:api', 'throttle:60,1')->group(function () {
-    Route::get('/profile', function () {
+Route::fallback(function () {
+    return 'Not match anything';
+});
+
+
+Route::domain('api.myapp.com')->group(function () {
+    Route::get('/', function () {
+    });
+});
+
+
+Route::domain('{account}.myapp.com')->group(function () {
+    Route::get('/', function ($account) {
+        //
+    });
+    Route::get('users/{id}', function ($account, $id) {
         //
     });
 });
+
