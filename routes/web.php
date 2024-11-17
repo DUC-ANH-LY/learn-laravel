d9c5fa599fcc745e91a62ed7ab8d5ad23d2458ab
DUC-ANH-LY <namobilegame@gmail.com>
2024-11-17 21:16:30 +0700

    ex 3.5

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 
-// with Facade
-// routes/web.php
 Route::get('/', function () {
     return 'Hello, World!';
 });
+Route::post('/', function () {
+    // Handle someone sending a POST request to this route
+});
+Route::put('/', function () {
+    // Handle someone sending a PUT request to this route
+});
+Route::delete('/', function () {
+    // Handle someone sending a DELETE request to this route
+});
+Route::any('/', function () {
+    // Handle any verb request to this route
+});
+Route::match(['get', 'post'], '/', function () {
+    // Handle GET or POST requests to this route
+});
 
 
-// without Facade
-//$router->get('/', function () {
-//    return 'Hello, World!';
-//});
 
-
-
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//Route::get('about', function () {
-//    return view('about');
-//});
-//Route::get('products', function () {
-//    return view('products');
-//});
-//Route::get('services', function () {
-//    return view('services');
-//});
