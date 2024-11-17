27a68ef527e4c552cc603455a9d5536903fd6993
DUC-ANH-LY <namobilegame@gmail.com>
2024-11-17 21:13:58 +0700

    ex 3.1 3.2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+// with Facade
+// routes/web.php
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello, World!';
 });
+
+
+// without Facade
+//$router->get('/', function () {
+//    return 'Hello, World!';
+//});
+
+
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//Route::get('about', function () {
+//    return view('about');
+//});
+//Route::get('products', function () {
+//    return view('products');
+//});
+//Route::get('services', function () {
+//    return view('services');
+//});
